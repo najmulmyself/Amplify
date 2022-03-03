2ac9d243b4610383b2360df161c0403169ae6d90
najmulmyself <najmul.myself@gmail.com>
2022-03-03 23:28:48 +0600

    amplify init

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:amplify/screen/home_Screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: HomeScreen(),
+    );
+  }
+}
